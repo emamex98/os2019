e6ed1fad095a4ee968399fa07f4905a265dcf3bb
emamex98 <emamex98@gmail.com>
2019-05-15 01:05:33 +0000

    Practica 6 files

--- pthread/threads.c
+++ pthread/threads.c
@@ -1,27 +1,21 @@
-/******************************************************************************
-* FILE: hello.c
-* DESCRIPTION:
-*   A "hello world" Pthreads program.  Demonstrates thread creation and
-*   termination.
-* AUTHOR: Blaise Barney
-* LAST REVISED: 08/09/11
-******************************************************************************/
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define NUM_THREADS	10
+#define NUM_THREADS	20
+
 pthread_mutex_t lock;
 
-int deposito = 0;
+int deposito = 100;
 
 void *PrintHello(void *threadid)
 {
-   long tid;
-   int localdeposito = deposito;gi
-   pthread_mutex_lock(&lock);
+  long tid;
+  int  localdeposito;
+  pthread_mutex_lock(&lock);
+  localdeposito = deposito;
    tid = (long)threadid;
    printf("Hello World! It's me, thread #%ld!\n", tid);
-   printf("another print \n");
+   printf("another print");
    deposito = localdeposito + 100;
    pthread_mutex_unlock(&lock);
    pthread_exit(NULL);
@@ -40,18 +34,16 @@ int main(int argc, char *argv[])
        exit(-1);
        }
      }
-	
-   for(t=0; t<NUM_THREADS; t++) {
-       rc = pthread_join(threads[t], NULL);  
+    for(t=0; t<NUM_THREADS; t++) {
+       rc = pthread_join(threads[t], NULL);
        if (rc) {
-          printf("ERROR; return code from pthread_create() is %d\n", rc);
+          printf("ERROR; return code from pthread_join() is %d\n", rc);
           exit(-1);
-	   }
-	}
-
-   printf("mi dinero es %d\n", deposito);
+          }
+       }
+ 
+   printf("mi dinero es %d\n",deposito);
    /* Last thing that main() should do */
    pthread_exit(NULL);
-	
-  return 0;
+
 }
